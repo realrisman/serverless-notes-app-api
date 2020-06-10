8626da5e553b4b871b8280e6ccc86e8871afce3e
Muhamad Risman <real.risman@gmail.com>
2020-06-10 22:04:46 +0700

    Fix some typo on get.js

--- get.js
+++ get.js
@@ -5,10 +5,10 @@ export const main = handler(async (event, context) => {
   const params = {
     TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool indentity id of the authenticated user
+    // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
     Key: {
-      userId: event.requestContext.identity.congnitoIdentityId,
+      userId: event.requestContext.identity.cognitoIdentityId,
       noteId: event.pathParameters.id,
     },
   };
